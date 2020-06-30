c00c6d072b3154a110d54884a9c6b411f81f3352
Cameron Nigro <cameron.nigro@gmail.com>
2020-06-30 17:49:15 -0400

    initial commit

--- cms.rb
+++ cms.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'tilt/erubis'
+
+root = File.expand_path('..', __FILE__)
 
 get '/' do
-  "Getting started."
+  # @files = Dir.entries('data').select { |file| !File.directory? file }
+  @files = Dir.glob(root + "/data/*").map { |path| File.basename(path) }
+  erb :index
+end
+
+get '/:filename' do |filename|
+  file_path = root + "/data/" + filename
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
+  # @text = File.readlines('data/' + filename)
+  # erb :contents
 end
