0aeb66d1ee8b7559026ec2b56b08f1f2cd8c1c86
Cameron Nigro <cameron.nigro@gmail.com>
2020-06-30 22:41:48 -0400

    add tests and handling for non-existent files

--- Gemfile
+++ Gemfile
@@ -3,5 +3,8 @@ source "https://rubygems.org"
 gem "sinatra", "~>2.0"
 gem "sinatra-contrib"
 gem "erubis"
+gem "rack-test"
+gem "minitest"
+gem "redcarpet"
 
 ruby "2.6.5"
--- cms.rb
+++ cms.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
 
 root = File.expand_path('..', __FILE__)
+
+configure do
+  enable :sessions
+  set :session_secret, "it's a secret to everybody"
+end
 
 get '/' do
   # @files = Dir.entries('data').select { |file| !File.directory? file }
@@ -12,8 +18,15 @@ end
 
 get '/:filename' do |filename|
   file_path = root + "/data/" + filename
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:message] = "#{filename} does not exist."
+    redirect '/'
+  end
+
   # @text = File.readlines('data/' + filename)
   # erb :contents
 end
